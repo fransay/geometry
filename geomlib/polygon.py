8803584ac4ca140501ed213ca474184b5f7fe70e
Francis Osei <francisannin5@gmail.com>
2022-03-08 15:15:02 +0000

    completed polygon

--- geomlib/polygon.py
+++ geomlib/polygon.py
@@ -8,25 +8,32 @@ class polygon(linearRing):
 
     def length(self):
         """the length of a polygon must be similar or the same as that of linearRing """
-        return linearRing.length()
+        return linearRing.length(self)
 
     def area(self):
         """the length of a polygon must be similar or the same as that of linearRing """
 
-        return linearRing.area()
+        return linearRing.area(self)
 
     def centriod(self):
         """the length of a polygon must be similar or the same as that of linearRing """
 
-        return linearRing.centriod()
+        return linearRing.centriod(self)
 
     def __str__(self):
         """WKR string representation"""
 
-        return linearRing.__str__()
+        return linearRing.__str__(self)
 
     # Unfinished and there is more to add!!!!
     # {
 
-    
+
     # }
+
+
+
+if __name__ == '__main__':
+    polygon1 = polygon([1, 3], [3, 5], [4, 5])
+    print(polygon1.area())
+    print(polygon1.length())
