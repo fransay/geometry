ca9f8d2c64bfa267540b2a45904eb3ee72b4ab62
Francis Osei <francisannin5@gmail.com>
2022-07-08 22:22:08 +0000

    refactored math error

--- 2D/tris/pyfuncs/centriod.py
+++ 2D/tris/pyfuncs/centriod.py
@@ -1,4 +1,4 @@
 def centriod(stn1: tuple, stn2: tuple, stn3: tuple):
     """return the cartesian coordinate of the centriod"""
-    return (stn1[0] + stn2[0] + stn3[0]) / 3, (stn1[1] / 3 + stn2[1] / 3 + stn3[1] / 3) / 3
+    return (stn1[0] + stn2[0] + stn3[0]) / 3, (stn1[1] + stn2[1] + stn3[1]) / 3
 
