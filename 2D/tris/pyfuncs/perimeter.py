28f74214a8f80078079fe268c08257dec88242d8
Francis Osei <francisannin5@gmail.com>
2022-07-08 19:41:28 +0000

    perimeter function complete

--- 2D/tris/pyfuncs/perimeter.py
+++ 2D/tris/pyfuncs/perimeter.py
@@ -2,12 +2,12 @@ from distance import distance
 
 
 def perimeter(stn1: tuple, stn2: tuple, stn3: tuple):
-    """returns perimeter when coordinates are given"""
+    """returns perimeter when cartesian coordinates are given"""
     return distance(stn1, stn2) + distance(stn2, stn3) + distance(stn3, stn1)
 
 
 def perimeter_dist(d1, d2, d3):
-    """perimeter when given distances"""
+    """returns perimeter when distances are given"""
     return d1+d2+d3
 
 
