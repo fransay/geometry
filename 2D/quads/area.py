ed402f456d16278e4ba07337eb9cf2cb30438f11
Francis Osei <francisannin5@gmail.com>
2022-07-16 21:34:57 +0000

    quad funcs begin

---
+++ 2D/quads/area.py
@@ -0,0 +1,10 @@
+def area_c():
+    """returns the area of a quadrilateral given coordinates"""
+    pass
+
+
+def area_d(length, breath):
+    """
+    returns the area of a quadrilateral given distances
+    """
+    return length * breath
