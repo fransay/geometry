a5b25b4e91c72df66bfd1e29b029ea0435d74e60
Francis Osei <francisannin5@gmail.com>
2022-07-29 09:09:59 +0000

    undefined error fixed

--- 2D/quads/perimeter.py
+++ 2D/quads/perimeter.py
@@ -6,6 +6,11 @@ def perimeter(stn1, stn2, stn3, stn4):
 
     def distance(pnt1, pnt2):
         """ptn must assume the form (x,y)"""
-        return s((pnt1[0] - pnt2[0]) - (pnt1[0] - pnt2[0]))
+        return s(abs((pnt1[0] - pnt2[0]) - (pnt1[1] - pnt2[1])))
 
-    return sum(distance(stn1, stn2) + distance(stn2, stn3) + distance(stn3,stn4) + distance(stn4, stn1))
+    return distance(stn1, stn2) + distance(stn2, stn3) + distance(stn3, stn4) + distance(stn1, stn4)
+
+
+
+if __name__ == '__main__':
+    print(perimeter([200, 893], [43, 6], [47, 7], [6, 8]))
